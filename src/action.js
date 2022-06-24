bb2152e8bdd9431d1186aa413c219cc5a5656d3d
Charlie Roche <charles.roche@avinode.com>
2022-06-25 00:46:11 +0200

    Added next step

--- src/action.js
+++ src/action.js
@@ -1,5 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run() {
-  console.log("You've won!");
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+  
+  await octokit.rest.issues.create({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Congratulations, You've won!",
+  });
 }
 
 run();
